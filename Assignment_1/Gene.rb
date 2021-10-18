d7bc06ed9467c631c7c1bd02d9531a7679e8c3ff
Oscar Fernandez <oscar.fernandez.blanco@alumnos.upm.es>
2021-10-17 20:18:44 -0400

    Copia2

--- Assignment_1/Gene.rb
+++ Assignment_1/Gene.rb
@@ -1,12 +1,22 @@
 class Gene  
   
+  #@@number_of_genes = 0
   attr_accessor :Gene_ID
   attr_accessor :Gene_name
   attr_accessor :mutant_phenotype
   
-  def initialize (params = {})
-    @Gene_ID = params.fetch(:Gene_ID, 'unknown')
-    @Gene_name = params.fetch(:Gene_name, 'unknown')
+  def initialize (params = {}) 
+    @Gene_ID = params.fetch(:Gene_ID, "unknown")
+    unless @geneid.match(/A[Tt]\d[Gg]\d\d\d\d\d/)
+      abort "Incorrect AGI code in Gene_ID (gene_information.tsv)"
+    end
+    @Gene_name = params.fetch(:Gene_name, "unknown")
     @mutant_phenotype = params.fetch(:mutant_phenotype, "")
   end
+  
+
+  #def how_many
+  #  return @@number_of_genes
+  #end
+    
 end
